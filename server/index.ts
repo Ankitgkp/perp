0fce29d799c0d2523091df37cd2471807ab2963f
Ankit Pandey <36405347+Ankitgkp@users.noreply.github.com>
2026-04-24 21:12:56 +0530

    Added basic boilderplate for tavily

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+import express, { response } from 'express';
+// const { tavily } = require('@tavily/core');
+import { tavily } from '@tavily/core'
+const app = express();
+app.use(express.json());
+
+const client = tavily({ apiKey: process.env.TAVILY_API_KEY })
+
+app.post('/chat', async (req, res) => {
+
+    const qurey = req.body.qurey;
+
+
+    const searchResponse = await client.search(qurey, {
+        searchDepth: 'advanced'
+    });
+
+    const searchResult = searchResponse.results;
+
+
+
+});
+
+
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+})
